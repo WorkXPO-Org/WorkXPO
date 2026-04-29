16d86f85a1cd143ca959319a849bd0d949dadcfd
rafinho0 <rafa06osilva@gmail.com>
2026-04-29 16:33:50 -0300

    mapped the items of the header and changed some styling attributes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
+import Header from "./components/Header";
+import './style.css'
+
 function App() {
-  return;
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,23 @@
+export default function Header() {
+  const MENU_ITEMS = [
+    { label: "Home", path: "/" },
+    { label: "Vitrine de Projetos", path: "/projects" },
+    { label: "Sobre", path: "/abbout" },
+  ];
+
+  return (
+    <header className="bg-primary p-5 border-b border-dark/20">
+      <nav>
+        <ul className="flex flex-row justify-center gap-10 text-xl font-semibold text-text-main">
+          {MENU_ITEMS.map((item) => (
+            <li key={item}>
+              <a href={item.path} className="hover:text-dark transition-colors">
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
